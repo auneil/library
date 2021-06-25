43371f496a205ff1ebbaf8290eade8dcea64437f
neil <auneil@hotmail.com>
2021-06-25 23:28:52 +1000

    Updated controller

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var wiki = require("./routes/wiki");
+var catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users/cool/", usersRouter);
 app.use("/wiki", wiki);
+app.use("/catalog", catalogRouter); // Add catalog routes to middleware chain.
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
